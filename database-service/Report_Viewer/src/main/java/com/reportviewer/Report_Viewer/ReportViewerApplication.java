1c999615d30912f16db0c8ba831238f08441dc3f
Flurin92 <flurin.stuebinger92@yahoo.de>
2025-01-08 16:14:32 +0100

    add Spring Boot Configuration

---
+++ database-service/Report_Viewer/src/main/java/com/reportviewer/Report_Viewer/ReportViewerApplication.java
@@ -0,0 +1,13 @@
+package com.reportviewer.Report_Viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportViewerApplication.class, args);
+	}
+
+}
---
+++ database-service/Report_Viewer/src/test/java/com/reportviewer/Report_Viewer/ReportViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reportviewer.Report_Viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
